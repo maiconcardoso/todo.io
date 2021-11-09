ad65ea9332a39d1176eb563664f1e7a57ec1deeb
Maicon Cardoso <maiconcardoso@hotmail.com>
2021-11-08 21:18:50 -0300

    adicionando formsGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from './models/todo.model';
 
 @Component({
@@ -9,8 +10,17 @@ import { Todo } from './models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: string = 'Minhas Tarefas';
+  //@ts-ignore
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
     this.todos.push(new Todo(1, 'Passear com o cachorro', false));
     this.todos.push(new Todo(2, 'ir ao supermercado', true));   
     this.todos.push(new Todo(3, 'Cortar o cabelo', false));
